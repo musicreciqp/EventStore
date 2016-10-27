dc7f5e5a3b7dfe66a9e8f2fa606fdd275e223072
Evin Ugur <etugur@wpi.edu>
2016-10-27 11:00:54 -0400

    Added a test DB

--- index.js
+++ index.js
@@ -29,3 +29,17 @@ app.get('/times', function(request, response) {
       result += i + ' ';
   response.send(result);
 });
+
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
