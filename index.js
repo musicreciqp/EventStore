74d230eed3debec6b85fa6f7da37504f3d05bcbd
Evin Ugur <etugur@wpi.edu>
2016-10-27 10:37:05 -0400

    times

--- index.js
+++ index.js
@@ -21,3 +21,11 @@ app.get('/cool', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
+});
