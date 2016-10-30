a405551040b2418975ae028db5005c9c7235f434
Evin Ugur <etugur@wpi.edu>
2016-10-30 11:32:43 -0400

    header

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.set('port', (process.env.PORT || 5000));
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
   next();
 });
 
