0d3e61bbab85f29e6296af47e5277a51cd58969b
Evin Ugur <etugur@wpi.edu>
2016-10-30 12:09:43 -0400

    '

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ app.post('/pandora-event', function(request, response) {
 	console.log(request.body.stationId);
 	console.log(request.body.songName);
 	console.log(request.body.songHref);*/
-	var sql = "INSERT into pandora_events values (" +request.body.event + ", " + request.body.username + ", " + request.body.stationId + ", " +
-			request.body.stationName + ", " + request.body.songName + ", " + request.body.songHref + ", " + request.body.shuffleEnabled + ", " + request.body.date + ")";
+	var sql = "INSERT into pandora_events values ('" +request.body.event + "', '" + request.body.username + "', " + request.body.stationId + ", '" +
+			request.body.stationName + "', '" + request.body.songName + "', '" + request.body.songHref + "', " + request.body.shuffleEnabled + ", " + request.body.date + ")";
 	console.log("SQL: " + sql);
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		client.query(sql, function (err, result) {
