87213161b7a24c694edbba213b56e7eae29607f1
Evin Ugur <etugur@wpi.edu>
2016-10-31 14:15:43 -0400

    bad char snuck in

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.all('/*', function(req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
-å
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
