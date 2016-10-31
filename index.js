66e8b58284b8fd481089cdfc2fcfc6117c367617
Evin Ugur <etugur@wpi.edu>
2016-10-31 14:13:48 -0400

    removed unused dependency

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var bodyParser = require('body-parser')
 var pg = require('pg'); 
@@ -12,22 +11,14 @@ app.all('/*', function(req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
-
-
+å
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.get('/', function(request, response) { response.render('pages/index'); });
+app.listen(app.get('port'), function() { console.log('Node app is running on port', app.get('port')) });
 
 app.post('/pandora-event', function(request, response) {
 	var sql = "INSERT into pandora_events (event, username, stationId, stationName, songName, songHref, shuffleEnabled, date) values ('" +request.body.event + "', '" + request.body.username + "', '" + request.body.stationId + "', '" +
@@ -37,7 +28,7 @@ app.post('/pandora-event', function(request, response) {
 		client.query(sql, function (err, result) {
 				done();
 				if (err) {console.log(err); response.send("Error " + err);}
-				else {response.send("Song Added " + cool());}
+				else {response.send("Song Added ");}
 			});
 	});
 });
@@ -46,10 +37,9 @@ app.get('/pandora_events', function(request, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM pandora_events', function(err, result) {
       done();
-      if (err)
-       { console.error(err); res.send("Error " + err); }
-      else
-		       {res.render('pages/pandora_events', {results: result.rows} ); } 
+      if (err) { console.error(err); res.send("Error " + err); }
+      else {res.render('pages/pandora_events', {results: result.rows} ); } 
 		 });
   });
 });
+
