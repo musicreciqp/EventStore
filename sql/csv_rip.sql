0aabf3056fd7666fd4bf7cb7897585f5186c5db9
Evin Ugur <etugur@wpi.edu>
2016-11-29 14:14:00 -0500

    order by clause in sql to csv

--- sql/csv_rip.sql
+++ sql/csv_rip.sql
@@ -1,4 +1,4 @@
 \copy (select * from users) to ~/Desktop/iqp/event_store/csv/users.csv DELIMITER ',' CSV HEADER;
-\copy (select * from pandora_events) to ~/Desktop/iqp/event_store/csv/pandora_events.csv DELIMITER ',' CSV HEADER;
-\copy (select * from tunein_discovery) to ~/Desktop/iqp/event_store/csv/tunein_discoveries.csv DELIMITER ',' CSV HEADER;
-\copy (select * from tunein_events) to ~/Desktop/iqp/event_store/csv/tunein_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events order by id) to ~/Desktop/iqp/event_store/csv/pandora_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from tunein_discovery order by id) to ~/Desktop/iqp/event_store/csv/tunein_discoveries.csv DELIMITER ',' CSV HEADER;
+\copy (select * from tunein_events order by id) to ~/Desktop/iqp/event_store/csv/tunein_events.csv DELIMITER ',' CSV HEADER;
