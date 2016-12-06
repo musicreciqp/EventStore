14054e370113fc91f607055e1849d92f94e0e609
Evin Ugur <etugur@wpi.edu>
2016-12-06 09:29:43 -0500

    Changed SQL to reflect Station Create

--- sql/pandora_exclusive_csv.sql
+++ sql/pandora_exclusive_csv.sql
@@ -1,1 +1,1 @@
-\copy (select * from pandora_events where event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/pandora_exclusive.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/pandora_exclusive.csv DELIMITER ',' CSV HEADER;
--- sql/users_csv.sql
+++ sql/users_csv.sql
@@ -1,29 +1,29 @@
-\copy (select * from pandora_events where username ='muiqp00@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/00_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp00@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/00_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp03@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/03_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp03@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/03_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp13@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/13_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp13@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/13_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp07@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/07_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp07@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/07_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp01@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/01_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp01@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/01_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp10@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/10_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp10@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/10_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp18@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/18_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp18@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/18_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp11@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/11_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp11@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/11_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp06@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/06_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp06@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/06_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp09@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/09_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp09@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/09_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp16@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/16_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp16@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/16_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp19@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/19_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp19@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/19_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp02@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/02_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp02@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/02_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp14@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/14_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp14@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/14_events.csv DELIMITER ',' CSV HEADER;
 
-\copy (select * from pandora_events where username ='muiqp12@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select') order by id) to ~/Desktop/EventStore/users/12_events.csv DELIMITER ',' CSV HEADER;
+\copy (select * from pandora_events where username ='muiqp12@hmamail.com' and event in ('Skip', 'Thumb Up Added', 'Thumb Up Deleted', 'Thumb Down Added', 'Thumb Down Deleted', 'Station Select', 'Station Create') order by id) to ~/Desktop/EventStore/users/12_events.csv DELIMITER ',' CSV HEADER;
