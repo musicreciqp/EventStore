96214147866a092e92440f60e62f02496769adae
Evin Ugur <etugur@wpi.edu>
2016-12-06 20:58:46 -0500

    Escape Apostrophe on Station Names for Postgres

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.post('/pandora-event', function(request, res) {
 app.post('/pandora/create', function(req, res) {
 	var username = req.body.username;
 	var event = 'Station Create';
-	var stationName = req.body.stationName;
+	var stationName = postgresQuoteEscape(req.body.stationName);
 	var stationId = req.body.stationId;
 	var daysAgo = Number(req.body.daysAgo);
 	var date = new Date();
